adc20a518bf635338c7e855eb67a66243fdaeb83
beapen <github@gulfdoctor.net>
2020-01-03 07:17:17 -0500

    start cui

---
+++ test/cuiSearch.test.js
@@ -0,0 +1,12 @@
+import CUISearch from '../src/function/cuiSearch'
+import API_KEY from './api'
+
+test('Get Results for a CUI', async () => {
+    const search1 = new CUISearch(API_KEY)
+    const CUI = 'C0009044'
+    search1.init(CUI)
+    await search1.query()
+    const result = search1.getResult()
+    expect(result.ui).toBe(CUI)
+})
+
