4ce41435975b01e5d98eef8d411fb8095001bee0
Bellraj Eapen <github@gulfdoctor.net>
2020-10-21 18:50:51 -0400

    Add license

--- src/model/queryTemplate.ts
+++ src/model/queryTemplate.ts
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2020 Bell Eapen
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
 import AtomModel from './atom'
 import DefinitionModel from './definition'
 import ConceptRelationModel from './conceptrelation'
